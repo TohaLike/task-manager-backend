75ac294e5c2d295c573eb9d24c70e1fee4cb8244
Anton Malyshev <antonpaper500@gmail.com>
2025-06-24 22:57:55 +0300

    updated

--- src/main.ts
+++ src/main.ts
@@ -51,6 +51,9 @@ async function bootstrap() {
 
   app.useWebSocketAdapter(redisIoAdapter);
 
+  console.log(config.getOrThrow<string>('ALLOWED_ORIGIN'))
+  console.log( config.getOrThrow<string>('SESSION_DOMAIN'))
+
   app.enableCors({
     origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
     credentials: true,
